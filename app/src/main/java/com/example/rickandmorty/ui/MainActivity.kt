c36685995a88ae433bab00e3582507e2a2a18e51
TTECH\ipek.birinci <ipek.birinci8@gmail.com>
2024-11-27 17:41:27 +0300

    add navigation between fragments and add binding to project

--- app/src/main/java/com/example/rickandmorty/ui/MainActivity.kt
+++ app/src/main/java/com/example/rickandmorty/ui/MainActivity.kt
@@ -3,13 +3,59 @@ package com.example.rickandmorty.ui
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.example.rickandmorty.R
+import com.example.rickandmorty.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+    private var noBackStack: Boolean = true
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         window.statusBarColor = ContextCompat.getColor(this, R.color.backGround)
 
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        changeBackgroundColor()
+        supportActionBar?.hide()
+
+        intent.extras?.getInt("destination")?.let {
+            openNavViewWithDestination(it)
+        } ?: run {
+            openNavViewWithDestination()
+        }
+    }
+
+    private fun openNavViewWithDestination(destination: Int? = null) {
+        val navView: BottomNavigationView = findViewById(R.id.bottomNavigationView)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navController = navHostFragment.navController
+
+        navView.setupWithNavController(navController)
+
+        destination?.let {
+            navView.selectedItemId = destination
+        }
+
+        navHostFragment.childFragmentManager.addOnBackStackChangedListener {
+            noBackStack = navHostFragment.childFragmentManager.backStackEntryCount == 0
+        }
+    }
+
+    fun changeBackgroundColor(isWhite: Boolean? = false) {
+        if (isWhite == true) {
+            window.statusBarColor = ContextCompat.getColor(this@MainActivity, R.color.backGround)
+        } else {
+            window.statusBarColor = ContextCompat.getColor(this@MainActivity, R.color.backGroundItem)
+        }
     }
 }
